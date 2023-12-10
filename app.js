9f3226233b1e3b467013d285834173f4eaae31f2
Fidelis0001 <72283400+Fidelis0001@users.noreply.github.com>
2023-12-10 18:12:56 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.use(cors());
+
+const deeplApiKey = process.env.DEEPL_API_KEY;
+
+app.post("/translate", async (req, res) => {
+  const { text } = req.body;
+  const languages = ["DE", "FR", "NL"]; // Languages to translate into
+  const translations = {};
+
+  for (const lang of languages) {
+    try {
+      const response = await axios.post(
+        "https://api-free.deepl.com/v2/translate",
+        `text=${text}&target_lang=${lang}&auth_key=${deeplApiKey}`,
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+        }
+      );
+
+      translations[lang] = response.data.translations[0].text;
+    } catch (error) {
+      console.error(`Error translating to ${lang}: ${error.message}`);
+      translations[lang] = "Translation not available";
+    }
+  }
+
+  res.json(translations);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
---
+++ script.js
@@ -0,0 +1,59 @@
+// script.js
+
+function copyTranslation(elementId) {
+  const translationField = document.getElementById(elementId);
+  const copyNotification = document.getElementById(
+    `copyNotification${elementId.slice(-2)}`
+  );
+
+  translationField.select();
+  document.execCommand("copy");
+
+  copyNotification.innerText = "Copied";
+  copyNotification.style.display = "block";
+
+  setTimeout(() => {
+    copyNotification.innerText = "";
+    copyNotification.style.display = "none";
+  }, 3000);
+}
+
+function attachEventListeners() {
+  const translateButton = document.getElementById("translate-btn");
+
+  // Attach the translateText function to the click event of the button
+  translateButton.addEventListener("click", translateText);
+
+  ["DE", "FR", "NL"].forEach((lang) => {
+    const copyButton = document.getElementById(`copyButton${lang}`);
+    copyButton.addEventListener("click", () =>
+      copyTranslation(`translation${lang}`)
+    );
+  });
+}
+
+async function translateText() {
+  const textToTranslate = document.getElementById("inputText").value;
+
+  try {
+    const response = await fetch("http://localhost:3000/translate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `text=${encodeURIComponent(textToTranslate)}`,
+    });
+
+    console.log("logs out", response);
+    const translations = await response.json();
+    document.getElementById("translationDE").value = translations["DE"];
+    document.getElementById("translationFR").value = translations["FR"];
+    document.getElementById("translationNL").value = translations["NL"];
+  } catch (error) {
+    console.error("Translation error:", error);
+    // Handle error
+  }
+}
+
+// Attach event listeners when the DOM content is fully loaded
+document.addEventListener("DOMContentLoaded", attachEventListeners);
